b409085e74506b91e66751d0c11d8eab734b58fc
enerehs101505-png <enerehs101505@gmail.com>
2026-03-11 10:06:46 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return jsonify({
+        "message": "Welcome to my Flask API!",
+        "student": "YOUR FULL NAME"
+    })
+
+@app.route("/about")
+def about():
+    return jsonify({
+        "project": "Flask API Deployment Activity",
+        "developer": "SHERENEBRITOS",
+        "status": "Running on Render"
+    })
+
+@app.route("/hello/<name>")
+def hello(name):
+    return jsonify({
+        "message": f"Hello {name}! Welcome to my API."
+    })
+
+if __name__ == "__main__":
+    app.run(debug=True)
