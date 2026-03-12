7fb3e6ea74da996d0c900603b014105ff92f522e
enerehs101505-png <enerehs101505@gmail.com>
2026-03-12 15:36:50 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,30 +1,23 @@
-from flask import Flask, jsonify, render_template
+
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-# Home page with UI
 @app.route("/")
 def home():
-    return render_template("index.html")
-
-# API: Home info
-@app.route("/api")
-def api_home():
     return jsonify({
         "message": "Welcome to my Flask API!",
         "student": "SHERENE BRITOS"
     })
 
-# API: About
 @app.route("/about")
 def about():
     return jsonify({
         "project": "Flask API Deployment Activity",
-        "developer": "SHERENE BRITOS",
+        "developer": "SHERENEBRITOS",
         "status": "Running on Render"
     })
 
-# API: Greeting
 @app.route("/hello/<name>")
 def hello(name):
     return jsonify({
@@ -32,4 +25,5 @@ def hello(name):
     })
 
 if __name__ == "__main__":
-    app.run(debug=True)
+
+    app.run(debug=True)        
